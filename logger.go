b23b4cd2df38d82932936fbb3a4ae0180539868b
jdxj <985759262@qq.com>
2020-09-17 09:54:43 +0800

    refactor: 重构初始化方式

--- logger.go
+++ logger.go
@@ -3,38 +3,71 @@ package logger
 import (
 	"os"
 
-	"gopkg.in/natefinch/lumberjack.v2"
-
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 var (
-	sugar *zap.SugaredLogger
+	desugar *zap.Logger
+	sugar   *zap.SugaredLogger
 )
 
-func Init(path, mode string) {
-	core := zapcore.NewCore(
-		encoder(mode),
-		syncer(path, mode),
-		level(mode),
-	)
-	sugar = zap.New(core).Sugar()
+type OptionFunc func(opts *Options)
+
+type Options struct {
+	Mode     string
+	FileName string
+
+	MaxSize    int
+	MaxAge     int
+	MaxBackups int
+
+	LocalTime bool
+	Compress  bool
 }
 
-func syncer(path, mode string) zapcore.WriteSyncer {
-	switch mode {
+func NewPathMode(path, mode string) {
+	optF := OptionFunc(func(opts *Options) {
+		opts.Mode = mode
+		opts.FileName = path
+		opts.MaxSize = 500
+		opts.MaxAge = 30
+		opts.MaxBackups = 10
+		opts.LocalTime = false
+		opts.Compress = false
+	})
+	New(optF)
+}
+
+func New(optsF ...OptionFunc) {
+	opts := new(Options)
+	for _, optF := range optsF {
+		optF(opts)
+	}
+
+	core := zapcore.NewCore(
+		encoder(opts.Mode),
+		syncer(opts),
+		level(opts.Mode),
+	)
+	desugar = zap.New(core)
+	sugar = desugar.Sugar()
+}
+
+func syncer(opts *Options) zapcore.WriteSyncer {
+	switch opts.Mode {
 	case "debug":
 		return zapcore.AddSync(os.Stdout)
 
 	case "release":
 		rotation := &lumberjack.Logger{
-			Filename:   path,
-			MaxSize:    100,
-			MaxAge:     30,
-			MaxBackups: 10,
-			LocalTime:  false,
-			Compress:   false,
+			Filename:   opts.FileName,
+			MaxSize:    opts.MaxSize,
+			MaxAge:     opts.MaxAge,
+			MaxBackups: opts.MaxBackups,
+			LocalTime:  opts.LocalTime,
+			Compress:   opts.Compress,
 		}
 		return zapcore.AddSync(rotation)
 	}
@@ -60,22 +93,22 @@ func level(mode string) zapcore.Level {
 	return zap.DebugLevel
 }
 
-func Debug(template string, args ...interface{}) {
+func Debugf(template string, args ...interface{}) {
 	sugar.Debugf(template, args...)
 }
 
-func Info(template string, args ...interface{}) {
+func Infof(template string, args ...interface{}) {
 	sugar.Infof(template, args...)
 }
 
-func Warn(template string, args ...interface{}) {
+func Warnf(template string, args ...interface{}) {
 	sugar.Warnf(template, args...)
 }
 
-func Error(template string, args ...interface{}) {
+func Errorf(template string, args ...interface{}) {
 	sugar.Errorf(template, args...)
 }
 
 func Sync() {
-	_ = sugar.Sync()
+	_ = desugar.Sync()
 }
